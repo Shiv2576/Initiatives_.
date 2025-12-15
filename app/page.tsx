f0261d1a924d0d0c3b4152f23ea32198730c0ed2
Shivang <dixitshiva12358@gmail.com>
2025-12-15 23:42:01 +0530

    initial

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import CTABanner from "@/components/cta-banner";
+import FAQ from "@/components/faq";
+import Features from "@/components/features";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import { Navbar } from "@/components/navbar";
+import Pricing from "@/components/pricing";
+import Testimonials from "@/components/testimonials";
+import Background from "@/components/Background";
+
+export default function Home() {
+  return (
+    <>
+      <Background />
+      <Navbar />
+      <main className="pt-16 xs:pt-20 sm:pt-24">
+        <Hero />
+        <Features />
+        <Pricing />
+        <FAQ />
+        <Testimonials />
+        <CTABanner />
+        <Footer />
+      </main>
+    </>
+  );
+}
